67d223890cd75c985757ac5815cbe2c3d62949ea
Sardius <0xsardius@gmail.com>
2023-08-28 20:16:40 -0400

    Completed interactions and interactions tests

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -5,7 +5,7 @@
 
 pragma solidity ^0.8.18;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 import {FundMe} from "../src/FundMe.sol";
 
@@ -13,7 +13,9 @@ contract FundFundMe is Script {
     uint256 constant SEND_VALUE = 0.1 ether;
 
     function fundFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
         FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
         console.log("Funded FundMe with %s", SEND_VALUE);
     }
 
@@ -22,10 +24,24 @@ contract FundFundMe is Script {
             "FundMe",
             block.chainid
         );
-        vm.startBroadcast();
+
         fundFundMe(mostRecentlyDeployed);
-        vm.stopBroadcast();
     }
 }
 
-contract WithdrawFundMe is Script {}
+contract WithdrawFundMe is Script {
+    function withdrawFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
+        vm.stopBroadcast();
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+
+        withdrawFundMe(mostRecentlyDeployed);
+    }
+}
--- test/integration/InteractionsTest.t.sol
+++ test/integration/InteractionsTest.t.sol
@@ -5,6 +5,7 @@ import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../../src/FundMe.sol";
 import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
 import {FundFundMe} from "../../script/Interactions.s.sol";
+import {WithdrawFundMe} from "../../script/Interactions.s.sol";
 
 contract InteractionsTest is Test {
     FundMe fundMe;
@@ -23,5 +24,9 @@ contract InteractionsTest is Test {
     function testUserCanFundInteractions() public {
         FundFundMe fundFundMe = new FundFundMe();
         fundFundMe.fundFundMe(address(fundMe));
+
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
+        assert(address(fundMe).balance == 0);
     }
 }
